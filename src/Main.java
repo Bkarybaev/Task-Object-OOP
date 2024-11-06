7b9e8cfe960e0c8fe27a949d510cdcf4ec97b7e3
Bkarybaev <baielkarybaev@gmail.com>
2024-11-06 14:14:36 +0600

    end commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+     MyClass myClass1 = new MyClass("Baiel" , "Karybaev",19,"KFC");
+     MyClass myClass2 = new MyClass("Kanchoro" ,21,new String[]{"oop","method","loop"});
+
+        System.out.println(myClass1.getInfoFirstUser());
+        System.out.println("----===----===----\n");
+        System.out.println(myClass2.getInfoSecondUser());
     }
 }
--- src/MyClass.java
+++ src/MyClass.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class MyClass {
+    String name;
+    String surName;
+    int age;
+    String[] lessons;
+    String food;
+
+    public MyClass(String name, String surName, int age,String food) {
+        this.name = name;
+        this.surName = surName;
+        this.age = age;
+        this.food = food;
+    }
+
+    public MyClass(String name, int age , String[] lessons){
+        this.name = name;
+        this.age = age;
+        this.lessons = lessons;
+    }
+
+    public String getInfoFirstUser(){
+        return String.format("""
+                Name      : %s
+                Sure Name : %s
+                Age       : %d
+                live Food : %s
+                """,name,surName,age,food);
+    }
+
+    public String getInfoSecondUser(){
+        return String.format("""
+                Name      : %s
+                Age       : %d
+                Lessons   : %s
+                """,name,age, Arrays.toString(lessons));
+    }
+
+
 }
